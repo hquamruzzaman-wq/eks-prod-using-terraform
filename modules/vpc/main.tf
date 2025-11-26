7886d8daf6ccbf83f847884840e98d08a5624ad9
hquamruzzaman-wq <hquamruzzaman@gmail.com>
2025-11-25 18:34:41 -0800

    Add files via upload

---
+++ modules/vpc/main.tf
@@ -0,0 +1,102 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "eks-prod-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "eks-prod-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.azs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index)
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                        = "eks-prod-public-${var.azs[count.index]}"
+    "kubernetes.io/role/elb"    = "1"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + length(var.azs))
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name                              = "eks-prod-private-${var.azs[count.index]}"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_eip" "nat" {
+  count  = length(var.azs)
+  domain = "vpc"
+
+  tags = {
+    Name = "eks-prod-nat-eip-${var.azs[count.index]}"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  count         = length(var.azs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "eks-prod-nat-${var.azs[count.index]}"
+  }
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "eks-prod-public-rt"
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.azs)
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
+
+  tags = {
+    Name = "eks-prod-private-rt-${var.azs[count.index]}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.azs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.azs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC."
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones to use."
+}
